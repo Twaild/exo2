cf60231f044fbace48ebaa54e3e5dae7fa6aa86b
Twaild <theowald@gmail.com>
2019-04-02 12:04:06 +0200

    init7

--- src/components/Presentation/Presentation.js
+++ src/components/Presentation/Presentation.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import photo from "../../assets/images/photo.jpg";
 import Card from "../../../node_modules/@material-ui/core/Card/Card";
 import CardContent from "../../../node_modules/@material-ui/core/CardContent";
